81036b18c36ca8d84a34e10d0f9ccd6866430925
hi-kondo <hi.kondo@thk.co.jp>
2018-09-03 15:45:58 +0900

    v1.0:fk master

---
+++ aero_teleop/src/ps_teleop_node.cc
@@ -0,0 +1,20 @@
+#include "aero_teleop/ps_teleop.hh"
+
+int main(int argc, char *argv[])
+{
+  ROS_INFO("initializing robot ...");
+  
+  ros::init(argc, argv, "teleop_joy");
+  ros::NodeHandle nh_param;
+  aero::teleop::ps_teleop::Ptr joy
+    (new aero::teleop::ps_teleop(nh_param));  
+
+  
+  ros::Rate r(10);
+  while (ros::ok()) {
+    ros::spinOnce();
+    ROS_INFO("debug");
+    joy->loop();
+    r.sleep();
+  }
+}
